c659a2f1631aef64d913d7cd0ae791c1fc58bd4a
Florian Matter <florianmatter@gmail.com>
2022-04-29 22:55:26 +0200

    some linting

--- zssw/get_zssw_data.py
+++ zssw/get_zssw_data.py
@@ -5,15 +5,17 @@ import csv
 
 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M")
 
-fp = urllib.request.urlopen("https://www.zssw.unibe.ch/usp/zms/templates/crowdmonitoring/_display-spaces-zssw.php")
+fp = urllib.request.urlopen(
+    "https://www.zssw.unibe.ch/usp/zms/templates/crowdmonitoring/_display-spaces-zssw.php"
+)
 mybytes = fp.read()
 tent = mybytes.decode("utf8")
 fp.close()
 
 soup = BeautifulSoup(tent, "html.parser")
 count_div = soup.find_all("div", {"class": "go-stop-display_footer"})[0]
-count, max_count = count_div.text.split(" von ") 
+count, max_count = count_div.text.split(" von ")
 
-with open("zssw_data.csv",'a') as fd:
+with open("zssw_data.csv", "a") as fd:
     writer = csv.writer(fd)
     writer.writerow([count, max_count, timestamp])
--- zssw/print_zssw_graph.py
+++ zssw/print_zssw_graph.py
@@ -3,14 +3,15 @@ import matplotlib.pyplot as plt
 from datetime import datetime
 import matplotlib.dates as mdates
 import numpy as np
+
 # import pandas_bokeh
 
-df = pd.read_csv("zssw_data.csv", parse_dates = ["date"])
+df = pd.read_csv("zssw_data.csv", parse_dates=["date"])
 
 df["time"] = df["date"].dt.time
 
 df["daydate"] = df["date"].dt.date
-df['day'] = df["date"].dt.weekday
+df["day"] = df["date"].dt.weekday
 
 
 daydic = {
@@ -20,27 +21,27 @@ daydic = {
     3: "Donnsti",
     4: "Fryti",
     5: "Samsti",
-    6: "Sunnti"
+    6: "Sunnti",
 }
 df["Tag"] = df.apply(lambda x: daydic[x["day"]], axis=1)
 
 datefmt = "%Y-%m-%d"
 timefmt = "%H:%M:%S"
 fullfmt = datefmt + " " + timefmt
-#filter earlier closing times
+# filter earlier closing times
 ten_to_eight_start = datetime.strptime("2021-07-05", datefmt)
 ten_to_eight_stop = datetime.strptime("2021-08-06", datefmt)
 # df = df[~((df["date"] > ten_to_eight) & (df["date"]) & (df["time"] > datetime(2016, 1, 1, 20, 15).time()))]
 df = df[(ten_to_eight_start > df["date"]) | (df["date"] > ten_to_eight_stop)]
-#start at HS2021
+# start at HS2021
 hs21 = datetime.strptime("2021-09-20", datefmt)
 df = df[hs21 < df["date"]]
 
 print(df)
 
-#filter mornings
+# filter mornings
 df = df[df["time"] > datetime(2016, 1, 1, 6, 45).time()]
-#filter evenings
+# filter evenings
 df = df[df["time"] < datetime(2016, 1, 1, 22, 15).time()]
 
 
@@ -51,7 +52,8 @@ def by_date(dataframe, year, month, day):
     dataframe.set_index("time", inplace=True)
     dataframe.plot()
     plt.show()
-    
+
+
 def by_day(dataframe, name="single_day"):
     dataframe = dataframe[["count", "time"]]
     dataframe = dataframe.groupby(by="time").mean()
@@ -59,8 +61,9 @@ def by_day(dataframe, name="single_day"):
     # dataframe.index = dataframe.apply(lambda x: datetime.combine(today, x["time"]), axis=1)
     dataframe.drop(columns=["time"], inplace=True)
     dataframe.plot()
-    plt.savefig(name+".png")
+    plt.savefig(name + ".png")
     plt.clf()
+
 
 # weekday statistics
 weekday = df[~df["Tag"].isin(["Samsti", "Sunnti"])]
@@ -78,5 +81,5 @@ plt.savefig("Today.png")
 plt.clf()
 
 piv = pd.pivot_table(df, index=["time"], columns=["Tag"], values=["count"])
-ax = piv.plot(figsize=(20,10), x_compat=True)
+ax = piv.plot(figsize=(20, 10), x_compat=True)
 plt.savefig("Overall.png")
